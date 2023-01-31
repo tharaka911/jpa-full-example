1254aacd477c631b7b14940623425a252cfb36d1
tharaka911 <tharakahitibandara@gmail.com>
2023-01-31 22:50:37 +0530

    configuration done

---
+++ src/main/java/com/inactcode/jpafullexample/JpaFullExampleApplication.java
@@ -0,0 +1,13 @@
+package com.inactcode.jpafullexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFullExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaFullExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inactcode/jpafullexample/JpaFullExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inactcode.jpafullexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFullExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
